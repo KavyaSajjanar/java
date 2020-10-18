883fc1ac449e27e3a97dc788360bf6ec272e21f7
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-18 21:04:02 +0530

    4th commit

---
+++ pgm3.java
@@ -0,0 +1,12 @@
+class Work{
+public static void main(String[] args)
+{
+System.out.println("great");
+write();
+write();
+write();
+}
+public static void write(){
+System.out.println("basic");
+}
+}
