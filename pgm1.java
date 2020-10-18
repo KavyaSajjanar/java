a4119f69884f5fa7028a2d62cc6635b8b8930a28
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-18 20:45:54 +0530

    second commit

---
+++ pgm1.java
@@ -0,0 +1,14 @@
+class Write{
+public static void main(String[] args)
+{
+System.out.println("Super hero");
+music();
+}
+public static void music(){
+System.out.println("playing well");
+drinking();
+}
+public static void drinking(){
+	System.out.println("requirements");
+}
+}
