a88af9bc4e4a6cf2da3dacea737f54953e9736b3
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-18 21:02:26 +0530

    third commit

---
+++ pgm2.java
@@ -0,0 +1,22 @@
+class Read{
+	public static void main(String[]args){
+		System.out.println("pure soul");
+sure();
+	}
+public static void sure(){
+	System.out.println("perfect");
+solid();
+}
+public static void solid(){
+	System.out.println("understand");
+liquid();
+}
+public static void liquid(){
+	System.out.println("call");
+gas();
+}
+public static void gas(){
+	System.out.println("manage");
+	
+}
+}
