c9f0e5dbc3b979605bc67eafc04d034e5b089d7c
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-19 19:03:46 +0530

    6th commit

---
+++ Famous.java
@@ -0,0 +1,31 @@
+public class Famous{
+	public static void main(String[] args)
+	{
+		System.out.println("System");
+byte adharCardNumber  ;
+adharCardNumber=10;
+short statesInWorld  ;
+statesInWorld=7117;
+int totalVillages;
+totalVillages=50000;
+long capacityOfHumanBrain ;
+capacityOfHumanBrain=123456717;
+float perimeterOfTriangle  ;
+perimeterOfTriangle=18.8f;
+double cosValue;
+cosValue=345.567d;
+boolean cancerIsDisease  ;
+cancerIsDisease=false;
+char level;
+level='A';
+System.out.println(adharCardNumber);
+System.out.println(statesInWorld);
+System.out.println(totalVillages);
+System.out.println(capacityOfHumanBrain);
+System.out.println(perimeterOfTriangle);
+System.out.println(cosValue);
+System.out.println(cancerIsDisease);
+System.out.println(level);
+	}
+}
+
