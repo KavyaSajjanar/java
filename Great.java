10938aaa1662f9a9a6683be0bd9369782222676f
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-19 19:03:00 +0530

    5th commit

---
+++ Great.java
@@ -0,0 +1,31 @@
+public class Great{
+	public static void main(String[] args)
+	{
+		System.out.println("Assumption");
+byte peopleInHome;
+peopleInHome=4;
+short sizeOfEarth;
+sizeOfEarth=1274;
+int coronaCasesInKarnataka;
+coronaCasesInKarnataka=766000;
+long amountVal;
+amountVal=1234987456;
+float intrestRate;
+intrestRate=12.78f;
+double sineVal;
+sineVal=123.456d;
+boolean tabletIsSweet;
+tabletIsSweet=false;
+char letter ;
+letter='B';
+System.out.println(peopleInHome);
+System.out.println(sizeOfEarth);
+System.out.println(coronaCasesInKarnataka);
+System.out.println(amountVal);
+System.out.println(intrestRate);
+System.out.println(sineVal);
+System.out.println(tabletIsSweet);
+System.out.println(letter);
+	}
+}
+
