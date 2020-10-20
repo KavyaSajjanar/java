388b080dc0a5c7126bcf8111f6251a14008ca941
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-20 18:40:59 +0530

    method commit

---
+++ Invite.java
@@ -0,0 +1,10 @@
+public class Invite{
+public static void main(String[] args)
+{
+wishing("welcome");
+
+}
+public static void wishing(String wishing){
+	System.out.println("wishing:" +wishing);
+}
+}
