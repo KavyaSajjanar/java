5faad82bf325299a7799390c997ca813d849dbb0
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-20 18:42:20 +0530

    method2

---
+++ People.java
@@ -0,0 +1,13 @@
+public class People{
+public static void main(String[] args)
+{
+email("kavya12@gmail.com");
+programmingLanguage("java");
+}
+public static void email(String email){
+System.out.println("email:" +email);
+}
+public static void programmingLanguage(String programmingLanguage){
+System.out.println("programmingLanguage:" +programmingLanguage);
+}
+}
