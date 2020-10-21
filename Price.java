dab175326d7e88dced1315caf10bd5b611819477
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-21 20:17:08 +0530

    array commit

---
+++ Price.java
@@ -0,0 +1,19 @@
+public class Marks{
+public static void main(String[] args)
+{
+	System.out.println("Details Of Student");
+}
+public static void arrayOfPercentages()
+{
+System.out.println("invoking method");
+float primaryPercentage=98.1f;
+float highSchoolPercentage=85.5f;
+float pucPercentage=75.3f;
+float degreePercentage=60.2f;
+float[] ={primaryPercentage,highSchoolPercentage,pucPercentage,degreePercentage};
+int sizeOfArray=percentages.length;
+System.out.println("Total elements=" +sizeOfArray);
+flaot elementAtIndex2=emails[2];
+System.out.println("elementAtIndex two=" +elementAtIndex2);
+}
+}
