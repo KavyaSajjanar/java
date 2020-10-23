89f3fb2e39ce2c40b4e1ea1ad8d8d208fad6efa5
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-23 23:47:28 +0530

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player{
+public static void run(String[] nameOfPlayer)
+{
+int numberOfNameOfPlayers=nameOfPlayer.length;
+System.out.println("count of players:" +numberOfNameOfPlayers);
+System.out.println("element at index 0" +nameOfPlayer[0]);
+System.out.println("element at index 1" +nameOfPlayer[1]);
+}
+}
---
+++ Score.java
@@ -0,0 +1,9 @@
+public class Score{
+	public static void main(String[] cricket)
+	{
+		System.out.println("JVM invokes main method");
+		Player.run(cricket);
+		System.out.println("exit from main");
+	}
+}
+	
