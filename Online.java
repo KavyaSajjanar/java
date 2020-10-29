549967df9c9fdafdbb335dad3fd9e1738e0ec0bb
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-10-29 23:53:20 +0530

    Delete Online.java

--- Online.java
+++
@@ -1,9 +0,0 @@
-public class Online{
-public static void main(String[] trainees){
-System.out.println("main method");
-System.out.println("trainees"+trainees);
-int sizeOfMainParam=trainees.length;
-System.out.println("sizeOfMainParam:"+sizeOfMainParam);
-System.out.println("exit main");
-}
-}
