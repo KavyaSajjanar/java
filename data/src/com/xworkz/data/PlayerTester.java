e1da90c35cab38b088ecbc8e780cd7e5e2487635
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-12-10 00:27:55 +0530

    DTO

---
+++ data/src/com/xworkz/data/PlayerTester.java
@@ -0,0 +1,29 @@
+package com.xworkz.data;
+import java.util.Arrays;
+
+import com.xworkz.data.dto.PlayerDTO;
+
+public class PlayerTester {
+
+	public static void main(String[] args) {
+		
+		String[] sport= {"cricket","Hockey","BasketBall"};
+		String awards[]= {"Arjuna"};
+		PlayerDTO playerDTO=new PlayerDTO();
+		playerDTO.setName("Satnam singh");
+		System.out.println(playerDTO.getName());
+		playerDTO.setCountry("India");
+		System.out.println(playerDTO.getCountry());
+		String sports=Arrays.toString(sport);
+		playerDTO.setSports(sport);
+		System.out.println(playerDTO.getSports());
+		playerDTO.setAwards(awards);
+		System.out.println(playerDTO.getAwards());
+
+	}
+
+
+
+	}
+
+
