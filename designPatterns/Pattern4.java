93b06ec15d0f5c16cac774ff8c1b029a81e9b885
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2020-12-10 00:42:22 +0530

    design pattern

---
+++ designPatterns/Pattern4.java
@@ -0,0 +1,24 @@
+package com.xworkz.designPatterns;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		int i,j;
+		
+		// TODO Auto-generated method stub
+for( i=5;i>=1;i--) {
+	for( j=1;j<=i;j++) {
+		
+			System.out.print("*");
+			
+		
+			
+				
+			}
+		
+	System.out.println();
+	}
+}
+	
+}
+
---
+++ designPatterns/Pattern5.java
@@ -0,0 +1,10 @@
+package com.xworkz.designPatterns;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
