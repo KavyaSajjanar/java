7e273db4a2bf1a8eb03c32fe1ea9129c8a68e17f
KavyaSajjanar <72921102+KavyaSajjanar@users.noreply.github.com>
2021-01-25 00:31:09 +0530

    licenseCommit

---
+++ dto/LicenseDTO.java
@@ -0,0 +1,75 @@
+package com.xworkz.license.dto;
+
+import com.xworkz.license.constants.IdProof;
+import com.xworkz.license.constants.VehicleType;
+
+public class LicenseDTO extends CommonDTO{
+	private IdProof idProof;
+	private String idProofNo;
+	private VehicleType type;
+	private boolean disability;
+	private boolean commercial;
+	
+	public LicenseDTO() {
+		System.out.println("invoked licenseDto");
+	}
+
+	public LicenseDTO(IdProof idProof, String idProofNo, VehicleType type, boolean disability, boolean commercial) {
+		super();
+		this.idProof = idProof;
+		this.idProofNo = idProofNo;
+		this.type = type;
+		this.disability = disability;
+		this.commercial = commercial;
+	}
+	
+	@Override
+	public String toString() {
+		return "LicenseDTO [idProof=" + idProof + ", idProofNo=" + idProofNo + ", type=" + type + ", disability="
+				+ disability + ", commercial=" + commercial + "]";
+	}
+
+	public IdProof getIdProof() {
+		return idProof;
+	}
+
+	public void setIdProof(IdProof idProof) {
+		this.idProof = idProof;
+	}
+
+	public String getIdProofNo() {
+		return idProofNo;
+	}
+
+	public void setIdProofNo(String idProofNo) {
+		this.idProofNo = idProofNo;
+	}
+
+	public VehicleType getType() {
+		return type;
+	}
+
+	public void setType(VehicleType type) {
+		this.type = type;
+	}
+
+	public boolean isDisability() {
+		return disability;
+	}
+
+	public void setDisability(boolean disability) {
+		this.disability = disability;
+	}
+
+	public boolean isCommercial() {
+		return commercial;
+	}
+
+	public void setCommercial(boolean commercial) {
+		this.commercial = commercial;
+	}
+	
+
+}
+
+
